754c0d0086096836402b8658540669c49dc0c067
Sarvesh <sarvesh.singh@mitwpu.edu.in>
2026-06-15 13:43:55 +0530

    add temp csrf Exemt and add login & logut parm in Swagger .

--- smart_summarizer/smart_summarizer/settings.py
+++ smart_summarizer/smart_summarizer/settings.py
@@ -147,7 +147,7 @@ CORS_ALLOW_ALL_ORIGINS = False
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
+        'user.backends.CsrfExemptSessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
